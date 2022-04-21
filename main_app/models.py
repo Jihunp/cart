77e9236ae7e0b62e1b46f0026c0f4f3ba0d78265
Jihunp <edwardpark.park@gmail.com>
2022-04-20 20:30:10 -0700

    superusers defined import hasher for more security

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,36 @@
+from tkinter.ttk import _TreeviewColumnDict
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+#create new user
+#create superuser
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Must have an email address.")
+        if not username:
+            raise ValueError("Must have a username")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(make_password(password))
+        user.save(using=self._db)
+        return user
+    def create_super_user(self, email, username, password):
+        user = self.create_user(
+            email=self.create_user(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
 
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=70, unique=True)
@@ -11,6 +42,8 @@ class User(AbstractBaseUser):
     is_staff = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=True)
     
+    objects =MyUserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
