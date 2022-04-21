b6f4b51d145b9e38d119e38cf5d6066c4f09443d
Jihunp <edwardpark.park@gmail.com>
2022-04-20 21:00:43 -0700

    superusers

--- djangocart/settings.py
+++ djangocart/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "main_app.User"
+
 
 # Application definition
 
@@ -79,8 +81,8 @@ WSGI_APPLICATION = 'djangocart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangocart',
     }
 }
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,3 @@
-from tkinter.ttk import _TreeviewColumnDict
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.hashers import make_password
@@ -8,7 +7,7 @@ from django.contrib.auth.hashers import make_password
 class MyUserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         if not email:
-            raise ValueError("Must have an email address.")
+            raise ValueError("Must have an email address")
         if not username:
             raise ValueError("Must have a username")
         user = self.model(
@@ -18,7 +17,7 @@ class MyUserManager(BaseUserManager):
         user.set_password(make_password(password))
         user.save(using=self._db)
         return user
-    def create_super_user(self, email, username, password):
+    def create_superuser(self, email, username, password):
         user = self.create_user(
             email=self.create_user(email),
             username=username,
@@ -35,17 +34,17 @@ class MyUserManager(BaseUserManager):
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=70, unique=True)
     username = models.CharField(max_length=30, unique=True)
-    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_true=True)
+    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
     last_login= models.DateTimeField(verbose_name="last login", auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=True)
     
-    objects =MyUserManager()
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.username
