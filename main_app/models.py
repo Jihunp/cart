511b190e268e18c96e4e53c223d9b282fc250d58
Jihunp <edwardpark.park@gmail.com>
2022-04-26 17:54:55 -0700

    Order model

--- main_app/models.py
+++ main_app/models.py
@@ -67,3 +67,16 @@ class Product(models.Model):
     name = models.CharField(max_length=250, null=True)
     price = models.FloatField()
     description = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_order = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
