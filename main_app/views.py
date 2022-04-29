bdbcc87d951f2426101351106f506929acbbe8b0
Jihunp <edwardpark.park@gmail.com>
2022-04-29 12:40:26 -0700

    set up foundation for dynamic data representation

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,15 @@ def store(request):
     return render(request, 'store.html', context)
 
 def cart(request):
-    context ={}
+    # query an object if it doesn't exist it creates one
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # empty list for now but will be updated in the future
+        items = []
+    context ={'items':items}
     return render(request, 'cart.html', context)
 
 def checkout(request):
