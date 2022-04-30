1e0e8b4f90b1d977622cb8daab82a49809d3b4ab
Jihunp <edwardpark.park@gmail.com>
2022-04-30 15:24:23 -0700

    add to cart event listener

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,1 +1,19 @@
 console.log("Is this connected correctly")
+
+// function for sending product to the cart
+const updateBtn = document.getElementsByClassName('cart-update')
+
+for(let i = 0; i <updateBtn.length; i++) {
+    updateBtn[i].addEventListener('click', function () {
+        const productId = this.dataset.productId
+        const clicker = this.dataset.clicker
+        console.log('productId:', productId, 'Clicker:', clicker)
+
+        console.log('USER:' user)
+        if (user == 'Stranger'){
+            console.log('User has no authentification')
+        }else{
+            console.log('User is successfully authenticated')
+        }
+    })
+}
