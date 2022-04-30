d60678d07a41c3560db4ba799e6d5b8aba2ac455
Jihunp <edwardpark.park@gmail.com>
2022-04-30 16:21:59 -0700

    testing javascript connection

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,16 +1,19 @@
-console.log("Is this connected correctly")
-
+console.log("this is a tester for my button")
 // function for sending product to the cart
-const updateBtn = document.getElementsByClassName('cart-update')
+const updateBtn = document.getElementById('cart-updated')
 
-for(let i = 0; i <updateBtn.length; i++) {
-    updateBtn[i].addEventListener('click', function () {
-        const productId = this.dataset.productId
-        const clicker = this.dataset.clicker
-        console.log('productId:', productId, 'Clicker:', clicker)
+function clicker() {
+    console.log("this is clicking")
+}
 
-        console.log('USER:' user)
-        if (user == 'Stranger'){
+for(let i = 0; i < updateBtn.length; i++) {
+    updateBtn[i].addEventListener('click', function(){
+        let productId = this.dataset.productId
+        let action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:', user)
+        if (user === 'Stranger'){
             console.log('User has no authentification')
         }else{
             console.log('User is successfully authenticated')
