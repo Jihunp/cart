435313c0f03bb38ce6786e861126f290454c0529
Jihunp <edwardpark.park@gmail.com>
2022-04-30 17:16:50 -0700

    csrf token from django

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,22 +1,43 @@
 console.log("this is a tester for my button")
 // function for sending product to the cart
-const updateBtn = document.getElementById('cart-updated')
+
+// need to fix my buttons
 
 function clicker() {
-    console.log("this is clicking")
+    for(let i = 0; i < updateBtn.length; i++) {
+        const updateBtn = document.getElementById('cart-updated')
+        updateBtn[i].addEventListener('click', function(){
+            let productId = this.dataset.productId
+            let action = this.dataset.action
+            console.log('productId:', productId, 'action:', action)
+    
+            console.log('USER:', user)
+            if (user === 'Stranger'){
+                console.log('User has no authentification')
+            }else{
+                console.log('User is successfully authenticated')
+            }
+        })
+    }
 }
+function UserOrderUpdate(productId, action){
+    console.log('User is successfully authenticated')
 
-for(let i = 0; i < updateBtn.length; i++) {
-    updateBtn[i].addEventListener('click', function(){
-        let productId = this.dataset.productId
-        let action = this.dataset.action
-        console.log('productId:', productId, 'action:', action)
+    let url = '/update_item/'
 
-        console.log('USER:', user)
-        if (user === 'Stranger'){
-            console.log('User has no authentification')
-        }else{
-            console.log('User is successfully authenticated')
-        }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action' :action})
+    })
+    .then((response)=> {
+        return response.json()
+    })
+
+    .then((data)=> {
+        console.log('data:', data)
     })
 }
